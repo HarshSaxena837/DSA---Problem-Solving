94a72b0792a24f37ab7d98ff615259c28ea0f3a0
HarshSaxena837 <95163845+HarshSaxena837@users.noreply.github.com>
2024-01-25 12:41:41 +0530

    Time: 236 ms (96.29%), Space: 179.4 MB (81.14%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1457-pseudo-palindromic-paths-in-a-binary-tree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1457-pseudo-palindromic-paths-in-a-binary-tree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -11,30 +11,32 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root, int& count, vector<int>& temp){
+    void solve(TreeNode* root, int& cnt, vector<int> &temp){
+//         base case
         if(root == NULL){
             return;
         }
         temp[root->val]++;
-        solve(root->left, count, temp);
-        solve(root->right, count, temp);
+        solve(root->left, cnt, temp);
+        solve(root->right, cnt, temp);
         if(root->left == NULL && root->right == NULL){
-            int check = 0;
+            int flag = 0;
             for(int i = 1; i<=9; i++){
-                if(temp[i]%2 != 0){
-                    check++;
+                if(temp[i] % 2 != 0){
+                    flag++;
                 }
             }
-            if(check<=1){
-                count++;
+            if(flag <= 1){
+                cnt++;
             }
         }
         temp[root->val]--;
+
     }
     int pseudoPalindromicPaths (TreeNode* root) {
-        int count = 0;
+        int cnt = 0;
         vector<int> temp(10, 0);
-        solve(root, count, temp);
-        return count;
+        solve(root, cnt, temp);
+        return cnt;
     }
 };
