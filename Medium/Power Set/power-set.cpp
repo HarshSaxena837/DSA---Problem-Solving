bfbfd7114619a98337e53fb0930dc0f11c63f5de
HarshSaxena837 <95163845+HarshSaxena837@users.noreply.github.com>
2024-02-26 20:29:49 +0530

    Updated Solution - GfG to GitHub

--- Medium/Power Set/power-set.cpp
+++ Medium/Power Set/power-set.cpp
@@ -5,19 +5,35 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 	public:
+	    void solve(string s, int i, vector<string>& ans, string op){
+	        
+	       // base case
+	       if(i >= s.size()){
+	           if(op.size() > 0){
+	               ans.push_back(op);
+	           }
+	           return;
+	       }
+	       
+	       //exclude 
+	       solve(s, i+1, ans, op);
+	       
+	       //include
+	       char ch = s[i];
+	       op.push_back(ch);
+	       solve(s, i+1, ans, op);
+	       
+	       
+	       
+	    }
 		vector<string> AllPossibleStrings(string s){
 		    // Code here
-		    vector<string>v;
-            int n=s.size();
-            for(int i=1;i<(1<<n);i++){
-                string ans="";
-                for(int j=0;j<n;j++){
-                    if((i>>j)&1){ans+=s[j];}
-                }
-                v.push_back(ans);
-            }
-            sort(v.begin(),v.end());
-            return v;
+		    vector<string> ans;
+		    string op = "";
+		    int i = 0;
+		    solve(s,i, ans, op);
+		    sort(ans.begin(), ans.end());
+		    return ans;
 		}
 };
 
